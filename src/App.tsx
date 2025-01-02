a8e649edd9a8600dc2c0980a14c919fd48eea8c5
iliaIsr <ilil.sbl91@gmail.com>
2025-01-02 23:26:30 +0300

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
-import {Technologies} from "./Technologies";
-import {Header} from "./Header";
-import {Footer} from "./Footer";
-
+import {Header} from "./components/Header";
+import {Navbar} from "./components/Navbar";
+import {Profile} from "./components/Profile";
 
 const App = () => {
     return (
-        <div>
+        <div className="app-wrapper">
             <Header/>
-            <Technologies/>
-            <Footer/>
-        </div>);
+            <Navbar/>
+            <Profile/>
+        </div>
+    );
 }
 
 
